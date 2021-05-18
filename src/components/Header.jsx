6433610205a8a6418a81d5bae54c5727139d4aba
fabiolorenzi <fabiolorenzi@gmail.com>
2021-05-18 23:23:11 +0100

    styled Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./Header.style.css";
 
 function Header() {
     return(
         <div className="header">
-            <NavLink to="/" exact>HOME</NavLink>
-            <NavLink to="/tutorial">TUTORIAL</NavLink>
-            <NavLink to="/calculator">CALCULATOR</NavLink>
-            <NavLink to="/contacts">CONTACTS</NavLink>
+            <NavLink to="/" exact className="menu1" activeClassName="activeMenu1">HOME</NavLink>
+            <NavLink to="/tutorial" className="menu1" activeClassName="activeMenu1">TUTORIAL</NavLink>
+            <NavLink to="/calculator" className="menu1" activeClassName="activeMenu1">CALCULATOR</NavLink>
+            <NavLink to="/contacts" className="menu1" activeClassName="activeMenu1">CONTACTS</NavLink>
         </div>
     );
 };
