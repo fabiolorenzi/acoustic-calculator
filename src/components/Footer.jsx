bc96c89b95ee39a63d4dfffec4f958d755ee3993
fabiolorenzi <fabiolorenzi@gmail.com>
2021-05-19 02:19:47 +0100

    implementing Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.style.css";
+
+function Footer() {
+    return(
+        <div id="footer">
+            <a href="https://fabiolorenzi.github.io/portfolio" target="_blank" rel="noreferrer">Made by F. Lorenzi</a>
+        </div>
+    );
+};
+
+export default Footer;
