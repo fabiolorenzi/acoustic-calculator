04f240022ad006dc70b2df936b697f4feca27d16
fabiolorenzi <fabiolorenzi@gmail.com>
2021-05-19 21:11:39 +0100

    fixed Header component media query bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,12 @@ import BurgerMenu from "./BurgerMenu.jsx";
 function Header() {
     return(
         <div className="header">
-            <NavLink to="/" exact className="menu1" activeClassName="activeMenu1">HOME</NavLink>
-            <NavLink to="/tutorial" className="menu1" activeClassName="activeMenu1">TUTORIAL</NavLink>
-            <NavLink to="/calculator" className="menu1" activeClassName="activeMenu1">CALCULATOR</NavLink>
-            <NavLink to="/contacts" className="menu1" activeClassName="activeMenu1">CONTACTS</NavLink>
+            <div className="bigMenu">
+                <NavLink to="/" exact className="menu1" activeClassName="activeMenu1">HOME</NavLink>
+                <NavLink to="/tutorial" className="menu1" activeClassName="activeMenu1">TUTORIAL</NavLink>
+                <NavLink to="/calculator" className="menu1" activeClassName="activeMenu1">CALCULATOR</NavLink>
+                <NavLink to="/contacts" className="menu1" activeClassName="activeMenu1">CONTACTS</NavLink>
+            </div>
             <BurgerMenu />
         </div>
     );
