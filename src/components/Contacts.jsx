3c07a76815bc7663c8077719e280f9f2b37a3cba
fabiolorenzi <fabiolorenzi@gmail.com>
2021-05-20 23:54:26 +0100

    styled Contacts component

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import Logo from "./img/logo_01.png";
 import "./Contacts.style.css";
 
 function Contacts() {
     return(
-        <div className="contacts"></div>
+        <div className="contacts">
+            <h1>CONTACTS</h1>
+            <div className="contactsBody">
+                <div id="logoSideContacts">
+                    <img src={Logo} alt="logo" />
+                </div>
+                <div id="contactsText">
+                    <p id="contactsTitle">To contact the developer:</p>
+                    <p>by email</p>
+                    <a
+                        href="mailto:fabiolorenzi13975@gmail.com"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="contactsLink"
+                    >
+                        fabiolorenzi13975@gmail.com
+                    </a>
+                    <p>through Github</p>
+                    <a
+                        href="https://github.com/fabiolorenzi"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="contactsLink"
+                    >
+                        https://github.com/fabiolorenzi
+                    </a>
+                    <p>through Linkedin</p>
+                    <a
+                        href="https://www.linkedin.com/in/fabio-lorenzi-6901421b3/"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="contactsLink"
+                    >
+                        https://www.linkedin.com/in/fabio-lorenzi-6901421b3/
+                    </a>
+                </div>
+            </div>
+        </div>
     );
 };
 
