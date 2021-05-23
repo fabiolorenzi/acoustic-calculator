2b9f6ed33ea72b8abfe176318f05dfb5262861ee
fabiolorenzi <fabiolorenzi@gmail.com>
2021-05-23 22:19:10 +0100

    styled ProgramSlot component

--- src/components/Calculators.jsx
+++ src/components/Calculators.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Calculators.style.css";
 import ProgramSlot from "./ProgramSlot.jsx";
+import FrequenceOfResonanceImage from "./img/frequenceOfResonance_400x240.jpg";
 
 function Calculators() {
     return(
         <div className="calculators">
             <h1>CALCULATORS</h1>
             <div className="calculatorsBody">
-                <NavLink to="/calculators/frequence-of-resonance">
-                    <ProgramSlot
-                        title="Frequence of Resonance Calculator"
-                        image=""
-                    />
-                </NavLink>
+                <div className="programSlotContainer">
+                    <NavLink to="/calculators/frequence-of-resonance">
+                        <ProgramSlot
+                            title="Frequence of Resonance Calculator"
+                            image={FrequenceOfResonanceImage}
+                        />
+                    </NavLink>
+                </div>
             </div>
         </div>
     );
