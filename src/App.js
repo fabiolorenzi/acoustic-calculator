4c9e4a6f3743555d99d1a825323f90475cf30009
fabiolorenzi <fabiolorenzi@gmail.com>
2021-05-24 21:14:50 +0100

    fixed bug in Home component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Home from "./components/Home.jsx";
 import Tutorials from "./components/Tutorials.jsx";
+import FrequenceOfResonanceTutorial from "./components/FrequenceOfResonanceTutorial.jsx";
 import Calculators from "./components/Calculators.jsx";
 import FrequenceOfResonance from "./components/FrequenceOfResonance.jsx"
 import Contacts from "./components/Contacts.jsx";
@@ -16,6 +17,7 @@ function App() {
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/tutorials" component={Tutorials} />
+                <Route path="/tutorials/frequence-of-resonance-tutorial" component={FrequenceOfResonanceTutorial} />
                 <Route exact path="/calculators" component={Calculators} />
                 <Route path="/calculators/frequence-of-resonance" component={FrequenceOfResonance} />
                 <Route path="/contacts" component={Contacts} />
---
+++ src/components/FrequenceOfResonanceTutorial.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./FrequenceOfResonanceTutorial.style.css";
+
+function FrequenceOfResonanceTutorial() {
+    return(
+        <div></div>
+    );
+};
+
+export default FrequenceOfResonanceTutorial;
